68c94a4677c2656ba05551366e5a441f1a861354
Vasiliy Shaitan <shaitan@kraud.ru>
2020-12-28 16:32:05 +0300

    add big template

---
+++ src/main/java/archive/d1000/d100/d20/d8/D1128.java
@@ -0,0 +1,65 @@
+package archive.d1000.d100.d20.d8;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class D1128 {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() throws IOException {
+
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner(new InputStreamReader(System.in));
+            out = new PrintWriter(new OutputStreamWriter(System.out));
+            //in = new FastScanner(new File(".in"));
+            //out = new PrintWriter(new File(".out"));
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(File f) {
+            try {
+                br = new BufferedReader(new FileReader(f));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FastScanner(InputStreamReader f) {
+            br = new BufferedReader(f);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] arg) {
+        new D1128().run();
+    }
+}
---
+++ src/main/java/archive/e1000/e400/e50/e7/E1457.java
@@ -0,0 +1,65 @@
+package archive.e1000.e400.e50.e7;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class E1457 {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() throws IOException {
+
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner(new InputStreamReader(System.in));
+            out = new PrintWriter(new OutputStreamWriter(System.out));
+            //in = new FastScanner(new File(".in"));
+            //out = new PrintWriter(new File(".out"));
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(File f) {
+            try {
+                br = new BufferedReader(new FileReader(f));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FastScanner(InputStreamReader f) {
+            br = new BufferedReader(f);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] arg) {
+        new E1457().run();
+    }
+}
