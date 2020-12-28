4daa1ef05ed9b12fb4233b2365573f2ca8fbf2f3
Vasiliy Shaitan <shaitan@kraud.ru>
2020-12-28 15:38:06 +0300

    add big template

---
+++ src/main/java/archive/a0/a300/a40/a1/A341.java
@@ -0,0 +1,65 @@
+package archive.a0.a300.a40.a1;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class A341 {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() throws IOException {
+
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner(new InputStreamReader(System.in));
+            out = new PrintWriter(new OutputStreamWriter(System.out));
+            //in = new FastScanner(new File(".in"));
+            //out = new PrintWriter(new File(".out"));
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(File f) {
+            try {
+                br = new BufferedReader(new FileReader(f));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FastScanner(InputStreamReader f) {
+            br = new BufferedReader(f);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] arg) {
+        new A341().run();
+    }
+}
