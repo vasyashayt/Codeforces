f0bdd02a4448fc714dd4de3ebc995765136a8bca
vasyashayt <vasyashayt@gmail.com>
2021-01-03 13:17:14 +0300

    add problem a546

--- src/main/java/archive/a0/a500/a40/a6/A546.java
+++ src/main/java/archive/a0/a500/a40/a6/A546.java
@@ -4,11 +4,25 @@ package archive.a0.a500.a40.a6;
 import java.io.*;
 import java.util.StringTokenizer;
 
+// Солдат и бананы
 public class A546 {
     FastScanner in;
     PrintWriter out;
 
     public void solve() throws IOException {
+        int k = in.nextInt();
+        int n = in.nextInt();
+        int w = in.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= w; i++) {
+            sum += i * k;
+        }
+        int res = sum - n;
+        if (res <= 0) {
+            out.println(0);
+        } else {
+            out.println(res);
+        }
 
     }
 
