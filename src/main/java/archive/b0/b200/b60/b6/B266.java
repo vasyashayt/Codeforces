38ca8acc27b6d2fb4ca186a3949619be403c5853
vasyashayt <vasyashayt@gmail.com>
2021-01-03 20:03:00 +0300

    add problem b266

--- src/main/java/archive/b0/b200/b60/b6/B266.java
+++ src/main/java/archive/b0/b200/b60/b6/B266.java
@@ -4,11 +4,40 @@ package archive.b0.b200.b60.b6;
 import java.io.*;
 import java.util.StringTokenizer;
 
+// Очередь в школе
 public class B266 {
     FastScanner in;
     PrintWriter out;
 
     public void solve() throws IOException {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        String str = in.next();
+        char[] chars = str.toCharArray();
+        if (n < 2) {
+            out.println(str);
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            char[] steps = new char[n];
+            for (int j = 0; j < n; j++) {
+                if (chars[j] == 'G') {
+                    steps[j] = 'G';
+                } else {
+                    if (j < n - 1 && chars[j + 1] == 'G') {
+                        steps[j] = 'G';
+                        steps[j + 1] = 'B';
+                        j++;
+                    } else {
+                        steps[j] = 'B';
+                    }
+                }
+            }
+            chars = steps;
+        }
+        for (int i = 0; i < n; i++) {
+            out.print(chars[i]);
+        }
 
     }
 
