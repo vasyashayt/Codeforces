8e01b7b3a22a70b92a96ceee5420135ea8e1e968
vasyashayt <53205975+vasyashayt@users.noreply.github.com>
2021-01-06 17:26:31 +0300

    add problem a110

--- src/main/java/archive/a0/a100/a10/a0/A110.java
+++ src/main/java/archive/a0/a100/a10/a0/A110.java
@@ -4,12 +4,33 @@ package archive.a0.a100.a10.a0;
 import java.io.*;
 import java.util.StringTokenizer;
 
+// Почти счастливое число
 public class A110 {
     FastScanner in;
     PrintWriter out;
 
     public void solve() throws IOException {
-
+        String str = in.next();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '4' || c == '7') {
+                count++;
+            }
+        }
+        boolean flag = true;
+        String w = String.valueOf(count);
+        for (int i = 0; i < w.length(); i++) {
+            if (!(w.charAt(i) == '4' || w.charAt(i) == '7')) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            out.println("YES");
+        } else {
+            out.println("NO");
+        }
     }
 
     public void run() {
