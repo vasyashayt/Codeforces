add77e8c43d9d44ff5bf0e3f440fd48ad2f0588e
Vasiliy Shaitan <shaitan@kraud.ru>
2021-01-11 10:07:40 +0300

    add problem a734

--- src/main/java/archive/a0/a700/a30/a4/A734.java
+++ src/main/java/archive/a0/a700/a30/a4/A734.java
@@ -4,12 +4,23 @@ package archive.a0.a700.a30.a4;
 import java.io.*;
 import java.util.StringTokenizer;
 
+// Антон и Даник
 public class A734 {
     FastScanner in;
     PrintWriter out;
 
     public void solve() throws IOException {
-
+        int n = in.nextInt();
+        String str = in.next();
+        int a = 0;
+        int d = 0;
+        for (int i = 0; i < n; i++) {
+            if (str.charAt(i) == 'A') a++;
+            else d++;
+        }
+        if (a > d) out.println("Anton");
+        else if (d > a) out.println("Danik");
+        else out.println("Friendship");
     }
 
     public void run() {
@@ -53,6 +64,7 @@ public class A734 {
             }
             return st.nextToken();
         }
+
 
         int nextInt() {
             return Integer.parseInt(next());
