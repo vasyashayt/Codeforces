5d54b738f81f8c494ba3a16b0c98fff5f2a61d2d
Vasiliy Shaitan <shaitan@kraud.ru>
2021-01-28 12:52:31 +0300

    added: codeforces templates

---
+++ src/main/java/templates/round/J.java
@@ -0,0 +1,71 @@
+package templates.round;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class J {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() {
+
+    }
+
+    public void run() {
+        in = new FastScanner(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        solve();
+
+        out.close();
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+
+        FastScanner(InputStreamReader f) {
+            br = new BufferedReader(f);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+    public static void main(String[] arg) {
+        new J().run();
+    }
+}
