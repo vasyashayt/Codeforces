a3bf1bea51ee7e1e4f1c0d96609b968a710b5f16
Vasiliy Shaitan <shaitan@kraud.ru>
2021-01-28 13:08:24 +0300

    added: Codeforces Beta Round #4 (Дивизион 2)

---
+++ src/main/java/contest/c1_99/c4/codeforces_beta_round_4_div2/A.java
@@ -0,0 +1,74 @@
+package contest.c1_99.c4.codeforces_beta_round_4_div2;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// Арбуз
+public class A {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() {
+        int w = in.nextInt();
+        if (w < 4 || w % 2 != 0) out.println("NO");
+        else out.println("YES");
+    }
+
+    public void run() {
+        in = new FastScanner(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        solve();
+
+        out.close();
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+
+        FastScanner(InputStreamReader f) {
+            br = new BufferedReader(f);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+    public static void main(String[] arg) {
+        new A().run();
+    }
+}
---
+++ src/main/java/contest/c1_99/c4/codeforces_beta_round_4_div2/B.java
@@ -0,0 +1,72 @@
+package contest.c1_99.c4.codeforces_beta_round_4_div2;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// Перед экзаменом
+public class B {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() {
+        //todo
+    }
+
+    public void run() {
+        in = new FastScanner(new InputStreamReader(System.in));
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        solve();
+
+        out.close();
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+
+        FastScanner(InputStreamReader f) {
+            br = new BufferedReader(f);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+    public static void main(String[] arg) {
+        new B().run();
+    }
+}
