06949aa1b1e24d4bb8c8f90b2642b1ea6189d5a0
Vasiliy Shaitan <shaitan@kraud.ru>
2021-02-10 16:27:21 +0300

    added: VK Cup 2012 Квалификационный раунд 1

--- src/main/java/contest/c100_199/c158/B.java
+++ src/main/java/contest/c100_199/c158/B.java
@@ -4,12 +4,61 @@ package contest.c100_199.c158;
 import java.io.*;
 import java.util.StringTokenizer;
 
+// Такси
 public class B {
     FastScanner in;
     PrintWriter out;
 
     public void solve() {
-        //todo
+        int n = in.nextInt();
+        int car = 0;
+        int one = 0;
+        int two = 0;
+        int three = 0;
+        for (int i = 0; i < n; i++) {
+            int k = in.nextInt();
+            switch (k) {
+                case 4:
+                    car++;
+                    break;
+                case 3:
+                    if (one > 0) {
+                        one--;
+                        car++;
+                    } else {
+                        three++;
+                    }
+                    break;
+                case 2:
+                    if (two > 0) {
+                        two--;
+                        car++;
+                    } else {
+                        two++;
+                    }
+                    break;
+                case 1:
+                    if (three > 0) {
+                        three--;
+                        car++;
+                    } else {
+                        one++;
+                    }
+                    break;
+                default: break;
+            }
+        }
+        car += three;
+        if (two > 0) {
+            one--;
+            one--;
+            car++;
+        }
+        while(one > 0) {
+            one -= 4;
+            car ++;
+        }
+        out.println(car);
     }
 
     public void run() {
